204e0e5c8a21da913a1fff5b919fa38f23d944e5
RizkiRamadhan30 <rizkiannajah2002@gmail.com>
2023-06-05 19:41:03 +0700

    edit jadwal dan edit stasiun

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,80 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Dashboard Admin</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HalamanDepan</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>">
+	<title>Dashboard Admin</title>
+	<link rel="icon" href="<?= base_url('assets/favicon.png') ?>" type="image/png">
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/datatables.css') ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/datatables-b4.css') ?>">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <a class="navbar-brand" href="#">Admin Panel </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?= base_url() ?>">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-  </div>
-</nav>
+	<?php include 'include/nav.php'; ?>
 
-<div class="container-fluid my-5">
-<div class="row">
+	<div class="container-fluid my-4">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header bg-primary text-white text-center">Daftar Stasiun</div>
+					<div class="card-body">
+						<div class="alert alert-primary">
+							<strong>Perhatian !</strong> Tombol " <strong> Hapus Semua Data </strong>" akan menghapus Semua Data di Stasiun dan <strong>Data tidak dapat kembali lagi</strong>
+						</div>
+						<a onclick="return confirm('Yakin ingin menghapus semua data Stasiun ?')"  href="<?= base_url('hapus/semua/stasiun') ?>" class="btn btn-primary" >Hapus Semua Data</a><br><br>
+						<table class="table table-striped table-bordered datatables">
+							<thead>
+								<tr>
+									<th>No</th>
+									<th>Nama Stasiun</th>
+									<th>Aksi</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php  $no = 1; ?>
+								<?php foreach ($stasiun as $st): ?>
+								<tr>
+									<td><?= $no++ ?></td>
+									<td><?= $st->nama_stasiun ?></td>
+									<td>
+										<div class='btn-group btn-group-sm'>
+											<a class="btn btn-danger" href="<?= base_url('hapusStasiun/'.$st->id) ?>">Hapus</a>
+											<a class="btn btn-primary" href="<?= base_url('admin/dashboard/edit/'.$st->id) ?>">Edit</a>
+										</div>	
+									</td>
+								</tr>
+								<?php endforeach ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-header bg-success text-center text-white">Form Tambah Stasiun</div>
+					<div class="card-body">
+						<form action="<?= base_url('tambahStasiun') ?>" method="POST">
+							<div class="form-group">
+								<label>Nama Stasiun</label>
+								<input type="text" class="form-control" name="stasiun" placeholder="Nama Stasiun">
+							</div>
 
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">Daftar Stasiun</div>
-            <div class="card-body">
-                <table class="table table-condensed table-hover">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Stasiun</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $no = 1; ?>
-                        <?php foreach ($stasiun as $st): ?>
-                        <tr>
-                            <td><?= $no++ ?></td>
-                            <td><?= $st->nama_stasiun ?></td>
-                            <td>
-                                <a class="text-danger"href="">Hapus</a>
-                                <a href="">Edit</a>
-                            </td>
-                        </tr>
-                        <?php endforeach ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card">
-<div class="card-header">Form Tambah Stasiun</div>
-<div class="card-body">
-<form action=""method="POST">
-        <div class="form-group">
-            <label>Nama Stasiun</label>
-            <input type="text"class="form-control"name="stasiun" placeholder="Nama Stasiun">
-        </div>
-        <button class="btn btn-success">Tambah Stasiun</button>
-    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+							<button class="btn btn-block btn-success">Tambah Staisun</button>
+						</form>	
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
-
-<script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
-    
-    </body>
-    </html> 
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+	<script src="<?= base_url('assets/js/datatables.js') ?>"></script>
+	<script src="<?= base_url('assets/js/datatables-b4.js') ?>"></script>
+	<?php include 'include/datatables.php'; ?>
+</body>
+</html>
