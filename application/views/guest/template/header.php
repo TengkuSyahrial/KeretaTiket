9562facfa04dd0b86c91be5a45d5cb193b00a30e
TengkuSyahrial <tengkusyahrial7@gmail.com>
2023-06-06 20:28:38 +0700

    kelola jadwal

--- application/views/guest/template/header.php
+++ application/views/guest/template/header.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title><?= $judul?></title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HalamanDepan</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="icon" href="<?= base_url('assets/favicon.png') ?>" type="image/png">
+	<title><?= $judul ?></title>
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <a class="navbar-brand" href="#">TIKET KERETA </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?= base_url() ?>">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('konfirmasi')?>">Konfirmasi Pembayaran</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<style>
+.jumbotron{
+	background:url('<?= base_url("assets/bg.png") ?>')
+}
+.bayangan{
+	text-shadow: 2px 3px 4px #3c3c3c;
+}
+</style>
+<body <?php if($this->uri->segment(1) === 'print'): ?>  onload="window.print()" <?php else: endif; ?>  >
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+	    <a class="navbar-brand" href="#">iKereta</a>
+	    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="<?= base_url() ?>">Home <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="<?= base_url('konfirmasi') ?>">Konfirmasi Pembayaran</a>
+	      </li>
+	    </ul>
+      <a class="nav-link text-primary" href="<?= base_url('login') ?>">Login</a>
+	  </div>
+	</nav>
